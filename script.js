1c1f194fbe0f081ca91c3f3b1ece9d82ac0e3ad7
Bartek Żmuda <132684557+bertsu02@users.noreply.github.com>
2024-03-18 16:25:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,8 @@
   const wheel = document.querySelector('.wheel');
   const startButton = document.querySelector('.button');
   const messageContainer = document.querySelector('.message-container');
-  const wheel2 = document.querySelector('.wheel2');
-  const startSecondButton = document.querySelector('.button2');
   let deg = 0;
-  let deg2 = 0;
+
   startButton.addEventListener('click', () => {
     startButton.style.pointerEvents = 'none';
     deg = Math.floor(1000 + Math.random() * 1000);
@@ -58,29 +56,6 @@
     return 'Dead center, RE-SPIN!';
   }
 
-  function determineArea2(deg2) {
-    const areas = [
-      { name: 'Better Luck Next Time!', startDeg: 0, endDeg: 67.49 },
-      { name: '👑MAXX WIN!!!👑', startDeg: 67.5, endDeg: 90 },
-      { name: 'Better Luck Next Time!', startDeg: 90.01, endDeg: 157.49 },
-      { name: 'MINI WIN!', startDeg: 157.5, endDeg: 180 },
-      { name: 'Better Luck Next Time!', startDeg: 180.01, endDeg: 247.49 },
-      { name: '💎MAJOR WIN!!💎', startDeg: 247.5, endDeg: 270 },
-      { name: 'Better Luck Next Time!', startDeg: 270.01, endDeg: 337.49},
-      { name: '💰MINOR WIN!💰', startDeg: 337.5, endDeg: 360 },
-
-    ];
-    deg2 = deg2 % 360; 
-
-    for (const area of areas) {
-      if (deg2 >= area.startDeg && deg2 < area.endDeg) {
-        return area.name;
-      }
-    }
-  
-    return 'No area found';
-  }
-
   function displayMessage(area) {
     const messageContainer = document.getElementById('message-container');
     if (area === 'Kevin Spin')  {
@@ -89,48 +64,10 @@
       messageContainer.style.display = 'block';
       proceedButton.innerText = 'Proceed';
       proceedButton.onclick = function() {
-        messageContainer.innerHTML = '';
-        document.querySelector('.marker').style.zIndex = '1001';
-        const newWheel = document.createElement('img'); 
-        newWheel.classList.add('wheel2');
-        newWheel.src = 'wheel2.png'; 
-        newWheel.style.width = '750px'; 
-        newWheel.style.height = '750px';
-        newWheel.style.marginTop = '15%'; 
-        messageContainer.appendChild(newWheel); 
-        messageContainer.appendChild(document.createElement('br'));
-        const newButton = document.createElement('img'); 
-        newButton.classList.add('button2');
-        newButton.src = 'button2.png';
-        newButton.onclick = function() {
-          startSecondButton.style.pointerEvents = 'none';
-          deg2 = Math.floor(1000 + Math.random() * 1000);
-          newWheel.style.transition = 'all 10s ease-out';
-          newWheel.style.transform = `rotate(${deg2}deg)`;
-        };
-        newWheel.addEventListener('transitionend', function() {
-          const newDeg = Math.floor(1000 + Math.random() * 1000); 
-          newWheel.style.transition = 'none'; 
-          newWheel.style.transform = `rotate(${newDeg}deg)`; 
-          const newArea = determineArea2(newDeg); 
-          displayMessage2(newArea); 
-          newWheel.style.display = 'block'; 
-
-      });
-        messageContainer.appendChild(newButton); 
-        const closeButton = document.createElement('button');
-        closeButton.classList.add('close-button');
-        closeButton.innerText = 'X';
-        closeButton.style.float = 'right';
-        closeButton.onclick = function() {
-          messageContainer.style.display = 'none';
-        };
-        messageContainer.appendChild(closeButton);
+        window.location.href = 'https://bertsu02.github.io/kevin-spin/';
       };
-
       messageContainer.appendChild(document.createElement('br'));
       messageContainer.appendChild(proceedButton);
-      
     } else {
       messageContainer.innerText = `${area}`;
       messageContainer.style.display = 'block';
@@ -139,13 +76,5 @@
       }, 3000);
     }
   }
-  function displayMessage2(area) {
-    const messageContainer = document.getElementById('message-container');
-    messageContainer.innerText = area;
-    messageContainer.style.display = 'block';
-    setTimeout(() => {
-      messageContainer.style.display = 'none';
-    }, 5000);
-}
   
 })();
