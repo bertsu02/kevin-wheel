c99cd00286b874fb6660a7bb6b6ec149bbbed3a4
Bartek Żmuda <132684557+bertsu02@users.noreply.github.com>
2024-04-01 22:47:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@
    function determineArea(deg) {
     const areas = [
       { name: 'Try Again', startDeg: 0, endDeg: 22.49 },
-      { name: 'Stake 20$ Buy 50/50', startDeg: 22.5, endDeg: 45 },
+      { name: 'BC.GAME 20$ Buy 50/50', startDeg: 22.5, endDeg: 45 },
       { name: 'Try Again', startDeg: 45.01, endDeg: 67.49 },
       { name: 'Kevin Spin', startDeg: 67.5, endDeg: 90 },
       { name: 'Try Again', startDeg: 90.01, endDeg: 112.49 },
@@ -37,7 +37,7 @@
       { name: 'Try Again', startDeg: 135.01, endDeg: 157.49 },
       { name: 'Runestake 50 Battle 30%', startDeg: 157.5, endDeg: 180 },
       { name: 'Try Again', startDeg: 180.01, endDeg: 202.49 },
-      { name: 'Stake 5$ Tip', startDeg: 202.5, endDeg: 225 },
+      { name: 'BC.GAME 5$ Tip', startDeg: 202.5, endDeg: 225 },
       { name: 'Try Again', startDeg: 225.01, endDeg: 247.49 },
       { name: 'Daddyskins 10$ Depo', startDeg: 247.5, endDeg: 270 },
       { name: 'Try Again', startDeg: 270.01, endDeg: 292.49 },
